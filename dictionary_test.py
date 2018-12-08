8c360f3098dba9e16d27865ca366136e91fea1cc
K Murphy <kemurphy@canterburyschool.org>
2018-12-08 09:54:58 -0500

    Finished and working dictionary_test.py that reads form file and stores as dict

--- dictionary_test.py
+++ dictionary_test.py
@@ -6,13 +6,19 @@ exactly key-value pairs
 """
 import os
 import csv
+my_dict = dict()
 
 my_dir = os.path.dirname(os.path.realpath(__file__))
+
 filename = 'dictionary_test.csv'
 
 # TODO: read the data from the csv file and put into dictionary
-
-
+with open(my_dir + '/' + filename) as infile:
+    reader = csv.reader(infile)
+    for row in reader:
+        key = int(row[0])
+        value = row[1].strip()
+        my_dict[key] = value
 # print(read_data)
 
 
